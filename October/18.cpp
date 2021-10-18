90f048ad19e3e20198b497e71331c56b051a9911
Blazikengr8 <abhi.1414singh@gmail.com>
2021-10-18 11:27:50 +0530

    done

---
+++ October/18.cpp
@@ -0,0 +1,84 @@
+// https://practice.geeksforgeeks.org/problems/segregate-even-and-odd-nodes-in-a-linked-list5035/1#
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+//User function template for C++
+
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution{
+public:
+    Node* divide(int N, Node *head){
+         Node* even = new Node(INT_MIN);
+        Node* odd = new Node(INT_MIN);
+        Node *evenptr = even,*oddptr = odd;
+        Node* temp = head;
+        while(temp != NULL){
+            if(temp->data%2 == 0){
+                evenptr->next = temp;
+                evenptr = evenptr->next;
+            }
+            else{
+                oddptr->next = temp;
+                oddptr = oddptr->next;
+            }
+            temp = temp->next;
+        }
+        
+        evenptr->next = odd->next;
+        oddptr->next = NULL;
+        return even->next;
+    }
+};
+int main() {
+    //code
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < N-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        Node *ans = ob.divide(N, head);
+        printList(ans); 
+    }
+    return 0;
+}
