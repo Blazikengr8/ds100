5367a37705eff4e67cc4e4b68fcf18d7478812db
Blazikengr8 <abhi.1414singh@gmail.com>
2021-11-28 12:40:21 +0530

    try again

---
+++ November/28.cpp
@@ -0,0 +1,69 @@
+// https://practice.geeksforgeeks.org/problems/87f12e5c728d69a5322634776b54c75897d14daa/1
+#include<bits/stdc++.h>
+using namespace std;
+ 
+/* Link list node */
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+void push(struct Node** head_ref, int new_data)
+{
+   
+    struct Node* new_node = new Node(new_data);
+    
+    new_node->next = (*head_ref);
+    (*head_ref)    = new_node;
+} 
+
+int countTriplets(struct Node* head, int x) ;
+
+/* Driver program to test count function*/
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n , x ,i , num;
+        struct Node *head = NULL;
+        cin>>n>>x;
+        for(i=0;i<n;i++)
+        {
+            cin>>num;
+            push(&head,num);
+        }
+ 
+    /* Check the count function */
+    cout <<countTriplets(head, x)<< endl;
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+//User function Template for C++
+
+int countTriplets(struct Node* head, int x) 
+{ 
+    unordered_map<int,int> mp;
+    Node* node=head;
+    int res=0;
+    while(node->next) {
+        Node* temp=node->next;
+        while(temp) {
+            if(mp.find(x-node->data-temp->data)!=mp.end()) res++;
+            temp=temp->next;
+        }
+        mp[node->data]++;
+        node=node->next;
+    }
+    return res;
+} 
